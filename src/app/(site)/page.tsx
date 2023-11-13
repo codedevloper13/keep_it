2b2a9017cef9bbda80a9370b15f5b59546b5dd15
codedevloper13 <codedevloper13@gmail.com>
2023-11-13 15:07:32 +0530

    Added Client corousel section

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -5,11 +5,12 @@ import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import React, { Fragment } from "react";
 import Banner from "../../../public/appBanner.png";
+import { CLIENTS } from "@/lib/constants";
 
 const HomePage = () => {
 	return (
-		<section>
-			<div className='overflow-hidden px-4 sm:px-6 mt-10 sm:flex sm:flex-col gap-4 md:justify-center md:items-center'>
+		<>
+			<section className='overflow-hidden px-4 sm:px-6 mt-10 sm:flex sm:flex-col gap-4 md:justify-center md:items-center'>
 				<TitleSection
 					pill='✨ Optimized Workspace Experience'
 					title={
@@ -34,8 +35,49 @@ const HomePage = () => {
 				<div className='md:mt-[-40px] sm:w-full w-[750px] flex justify-center items-center mt-[-40px] relative sm:ml-0 ml-[-50px]'>
 					<Image src='/appBanner.png' alt='Application Banner' width={1359} height={710} unoptimized />
 				</div>
-			</div>
-		</section>
+			</section>
+			{/* Client Slider Section Start */}
+			<section className='relative'>
+				<div
+					className="overflow-hidden
+          flex
+          after:content['']
+          after:dark:from-brand-dark
+          after:to-transparent
+          after:from-background
+          after:bg-gradient-to-l
+          after:right-0
+          after:bottom-0
+          after:top-0
+          after:w-20
+          after:z-10
+          after:absolute
+
+          before:content['']
+          before:dark:from-brand-dark
+          before:to-transparent
+          before:from-background
+          before:bg-gradient-to-r
+          before:left-0
+          before:top-0
+          before:bottom-0
+          before:w-20
+          before:z-10
+          before:absolute
+				">
+					{[...Array(2)].map((arr) => (
+						<div key={arr} className='flex flex-nowrap animate-slide'>
+							{CLIENTS.map((client) => (
+								<div key={client.alt} className='relative w-[200px] m-20 shrink-0 flex items-center'>
+									<Image src={client.logo} alt={client.alt} width={150} className='object-contain max-w-none' unoptimized />
+								</div>
+							))}
+						</div>
+					))}
+				</div>
+			</section>
+			{/* Client Slider Section End */}
+		</>
 	);
 };
 
